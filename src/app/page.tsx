7dae02366e481483db84775aa407611c3d16b9d0
Pp <gamerer16@gmail.com>
2024-08-29 14:56:30 +0700

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,29 +3,42 @@ import Controller from "@components/Controller";
 import { Detail } from "@components/Detail";
 import { Header } from "@components/Header";
 
+import { useState } from "react";
+import { ThemeContext } from "@contexts/ThemeContext";
+
 export default function Home() {
   const THEMES = {
     LIGHT: {
-      name: "light",
+      name: "light-theme",
       bgColor: "white",
       fgColor: "black",
     },
     DARK: {
-      name: "dark",
+      name: "dark-theme",
       bgColor: "black",
       fgColor: "white",
     },
   };
-
-  const setLightTheme = () => {};
-  const setDarkTheme = () => {};
-  const toggleTheme = () => {};
+  const [theme, setTheme] = useState(THEMES.DARK);
+  const setLightTheme = () => { setTheme(THEMES.LIGHT); };
+  const setDarkTheme = () => { setTheme(THEMES.DARK); };
+  const toggleTheme = () => {
+    setTheme(theme.name === THEMES.LIGHT.name? THEMES.DARK : THEMES.LIGHT);
+  };
 
   return (
-    <div>
-      <Header />
-      <Detail />
-      <Controller />
+    <div style={{backgroundColor: theme.bgColor, minHeight: "100vh"}}> 
+     {/* Set Provider */}
+      <ThemeContext.Provider value={{
+        theme:theme,
+        setLightTheme, 
+        setDarkTheme, 
+        toggleTheme
+      }}>
+        <Header />
+        <Detail />
+        <Controller />
+      </ThemeContext.Provider>
     </div>
   );
 }
